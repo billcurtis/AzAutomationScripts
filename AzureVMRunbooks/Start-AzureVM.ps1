c799929fc47cbe9decf47867554b0e6690d0d0b6
Bill Curtis <{ID}+{username}@users.noreply.github.com>
2021-03-19 12:24:59 -0400

    Initial Commit

---
+++ AzureVMRunbooks/Start-AzureVM.ps1
@@ -0,0 +1,89 @@
+<#
+    .DESCRIPTION
+       Starts a Azure Virtual Machine
+
+
+    .INPUTS
+
+        ResourceGroupName - Resource Group name of the resource.
+
+        VMName - Name of the resource.
+
+
+    .NOTES
+    
+        Outputs whether or not the VM Start was successful.
+
+#>
+
+param (
+
+    [string]$ResourceGroupName,
+    [string]$VMName
+
+)
+
+
+# Import Modules
+
+Import-Module Az.Compute
+Import-Module Az.Accounts
+
+# Preferences
+
+$ErrorActionPreference = "Stop"
+$VerbosePreference = "Continue"
+
+
+$connectionName = "AzureRunAsConnection"
+try {
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection = Get-AutomationConnection -Name $connectionName         
+
+    Add-AzAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint `
+        | Out-Null
+}
+catch {
+    if (!$servicePrincipalConnection) {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    }
+    else {
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
+
+# Inputs
+Write-Verbose "Inputs are ResourceGroupName = $ResourceGroupName, VMName = $VMName "
+
+
+try {
+
+    # Start Virtual Machine
+
+    $params = @{
+
+        ResourceGroupName = $ResourceGroupName
+        Name              = $VMName
+
+    }
+
+    $output = Start-AzVM @params
+
+    $output.Status
+
+}
+
+catch {
+
+    Write-Verbose "Exception thrown: $($_.Exception.Message)"
+    Write-Output "Failed"
+
+}
+
+ 
